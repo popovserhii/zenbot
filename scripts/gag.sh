f7a83dc16c21bfef4f1de712315cfc4ebbe07dea
Serhii Popov <popow.serhii@gmail.com>
2022-02-21 06:00:51 +0200

    Upgrade Node.js from 10 to 17

--- scripts/gag.sh
+++ scripts/gag.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apk add --update npm git python make g++ util-linux
+#apk add --update npm git python make g++ util-linux
 
 # https://unix.stackexchange.com/a/332658/378971
 #ln -s /usr/local/bin/python3.6 /usr/local/bin/python3
@@ -9,9 +9,6 @@ apk add --update npm git python make g++ util-linux
 #rm package-lock.json
 #rm -Rf node_modules/
 #npm install
-
-
-#apk add git python make g++ apk util-linux
 
 while true
 do
